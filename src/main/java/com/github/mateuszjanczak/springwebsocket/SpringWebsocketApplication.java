203af5a00cf6eba7e2e9bdd2ca7bddcdfc0a7ab8
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-02-27 23:49:31 +0100

    Initial commit

---
+++ src/main/java/com/github/mateuszjanczak/springwebsocket/SpringWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.github.mateuszjanczak.springwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsocketApplication.class, args);
+    }
+
+}
